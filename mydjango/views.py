8359eb9627d14c9ec3c9864e793aea6c975dd9d6
Lyudmyla <kaluzhynova@gmail.com>
2017-01-06 14:38:41 +0200

    email

--- mydjango/views.py
+++ mydjango/views.py
@@ -16,6 +16,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.core.mail import send_mail
 from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 
 
 
