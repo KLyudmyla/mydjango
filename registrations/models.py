34d788a98480a2a3de8d024749ffabbd3ad4192b
Lyudmyla <kaluzhynova@gmail.com>
2017-01-09 20:22:57 +0200

    userprofile

--- registrations/models.py
+++ registrations/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from django.conf import settings
 
 
-class Users(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    password1 = models.TextField()
-    password2 = models.TextField()     
-    
+    activation_key = models.CharField(max_length=40, blank=True)
+    key_expires = models.DateTimeField(default=datetime.date.today())
     def __str__(self):
         return self.user.username
+
+class Meta:
+verbose_name_plural=u'User profiles'
