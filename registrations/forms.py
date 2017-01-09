4f8ed4fea01eafa57819d66722b0cc4261274777
Lyudmyla <kaluzhynova@gmail.com>
2017-01-09 20:40:37 +0200

    userprofile_model4

--- registrations/forms.py
+++ registrations/forms.py
@@ -2,12 +2,12 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Users
+from .models import UserProfile
 
 class UserCreateForm(UserCreationForm):
 #    email = forms.EmailField(required=True)
     class Meta:
-        model = User
+        model = UserProfile
         fields = ("username", "first_name", "last_name", "email", "password1", "password2")
         widgets = {
     #        'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Set password'}),
